b480c9c863634fdf326136294d8eeaaa754f7f23
bhargavinayudu88 <119724166+bhargavinayudu88@users.noreply.github.com>
2023-05-24 10:14:08 +0530

    s=int(input()) while(s):     n,a,b,k=map(int,input().split())     c1=0     c2=0     c=0     c1=n//a     c2=n//b     c=n//(a*b)     if(c1+c2-c>=k):         print('Win')     else:         print('Lose')     s-=1

---
+++ Appy_and_Contest.py
@@ -0,0 +1,14 @@
+s=int(input())
+while(s):
+    n,a,b,k=map(int,input().split())
+    c1=0
+    c2=0
+    c=0
+    c1=n//a
+    c2=n//b
+    c=n//(a*b)
+    if(c1+c2-c>=k):
+        print('Win')
+    else:
+        print('Lose')
+    s-=1
