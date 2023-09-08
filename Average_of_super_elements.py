75ae766f2d2ddd88e1d0cdd6f5ddeed8fb6f1151
bhargavinayudu88 <119724166+bhargavinayudu88@users.noreply.github.com>
2023-09-08 18:35:06 +0530

    n=int(input()) l=list(map(int,input().split())) v=[] for i in range(len(l)):     c=1     if (l[i]==-1):         continue     for j in range(len(l)):         if(i!=j and l[i]==l[j]):             c+=1             l[j]=-1     if(c==l[i]):         v.append(l[i]) if(len(v)>0):     s=sum(v)/len(v)     print("%.2f"%s) else:     print("-1")

--- Average_of_super_elements.py
+++ Average_of_super_elements.py
@@ -1,11 +1,19 @@
 n=int(input())
-a=list(map(int,input().split()))
-b=[]
-for i in a:
-    if i  not in b and a.count(i)==i :
-        b.append(i)
-if len(b)==0:
-    print(-1)
-    
+l=list(map(int,input().split()))
+v=[]
+for i in range(len(l)):
+    c=1
+    if (l[i]==-1):
+        continue
+    for j in range(len(l)):
+        if(i!=j and l[i]==l[j]):
+            c+=1
+            l[j]=-1
+    if(c==l[i]):
+        v.append(l[i])
+if(len(v)>0):
+    s=sum(v)/len(v)
+    print("%.2f"%s)
 else:
-    print("{:.2f}".format(sum(b)/len(b)))
+    print("-1")
+    
